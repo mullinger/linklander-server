d8554abddbc5ab224f72ee98789eb1f3dfa990df
Manuel Vogel <manuel.vogel@gmx.de>
2014-11-24 22:54:28 +0100

    removed commented tests

--- src/test/java/de/lander/persistence/daos/PersistenceGatewayImplTest.java
+++ src/test/java/de/lander/persistence/daos/PersistenceGatewayImplTest.java
@@ -25,199 +25,169 @@ import de.lander.persistence.entities.Link;
  */
 public class PersistenceGatewayImplTest {
 
-    private PersistenceGatewayImpl classUnderTest;
+	private PersistenceGatewayImpl classUnderTest;
 
-    @Before
-    public void setUp() {
-		classUnderTest = new PersistenceGatewayImpl(new TestGraphDatabaseFactory().newImpermanentDatabase());
-    }
+	@Before
+	public void setUp() {
+		classUnderTest = new PersistenceGatewayImpl(
+				new TestGraphDatabaseFactory().newImpermanentDatabase());
+	}
 
-    /**
-     * @author mvogel
-     */
-    @Test
-    public void shouldPersistAndReadLinkByFullName() throws Exception {
-        // == prepare ==
-        String name = "Neo4j-Tutorial";
-        String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-        String title = "Neo4j-Tutorial-Title";
+	/**
+	 * @author mvogel
+	 */
+	@Test
+	public void shouldPersistAndReadLinkByFullName() throws Exception {
+		// == prepare ==
+		String name = "Neo4j-Tutorial";
+		String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
+		String title = "Neo4j-Tutorial-Title";
 
-        // == go ==
-        classUnderTest.addLink(name, url, title);
-        List<Link> resultLinks = classUnderTest.getLink(LinkProperty.NAME, name);
+		// == go ==
+		classUnderTest.addLink(name, url, title);
+		List<Link> resultLinks = classUnderTest
+				.getLink(LinkProperty.NAME, name);
 
-        // == verify ==
-        assertThat(resultLinks.size(), is(1));
-        Link resultLink = resultLinks.get(0);
-        assertThat(resultLink.getUrl(), is(url));
-        assertThat(resultLink.getTitle(), is(title));
-        assertThat(resultLink.getClicks(), is(0));
-        assertThat(resultLink.getScore(), is(0.0));
-    }
+		// == verify ==
+		assertThat(resultLinks.size(), is(1));
+		Link resultLink = resultLinks.get(0);
+		assertThat(resultLink.getUrl(), is(url));
+		assertThat(resultLink.getTitle(), is(title));
+		assertThat(resultLink.getClicks(), is(0));
+		assertThat(resultLink.getScore(), is(0.0));
+	}
 
-    /**
-     * @author mvogel
-     */
-    @Test
-    public void shouldPersistAndReadLinkBySubstringName() throws Exception {
-        // == prepare ==
-        String name = "Neo4j-Tuto";
-        String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-        String title = "Neo4j-Tutorial-Title";
+	/**
+	 * @author mvogel
+	 */
+	@Test
+	public void shouldPersistAndReadLinkBySubstringName() throws Exception {
+		// == prepare ==
+		String name = "Neo4j-Tuto";
+		String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
+		String title = "Neo4j-Tutorial-Title";
 
-        // == go ==
-        classUnderTest.addLink(name, url, title);
-        List<Link> resultLinks = classUnderTest.getLink(LinkProperty.NAME, name);
+		// == go ==
+		classUnderTest.addLink(name, url, title);
+		List<Link> resultLinks = classUnderTest
+				.getLink(LinkProperty.NAME, name);
 
-        // == verify ==
-        assertThat(resultLinks.size(), is(1));
-        Link resultLink = resultLinks.get(0);
-        assertThat(resultLink.getUrl(), is(url));
-        assertThat(resultLink.getTitle(), is(title));
-        assertThat(resultLink.getClicks(), is(0));
-        assertThat(resultLink.getScore(), is(0.0));
-    }
+		// == verify ==
+		assertThat(resultLinks.size(), is(1));
+		Link resultLink = resultLinks.get(0);
+		assertThat(resultLink.getUrl(), is(url));
+		assertThat(resultLink.getTitle(), is(title));
+		assertThat(resultLink.getClicks(), is(0));
+		assertThat(resultLink.getScore(), is(0.0));
+	}
 
-    /**
-     * @author mvogel
-     */
-    @Test
-    public void shouldPersistAndReadLinkByFullUrl() throws Exception {
-        // == prepare ==
-        String name = "Neo4j-Tutorial";
-        String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-        String title = "Neo4j-Tutorial-Title";
+	/**
+	 * @author mvogel
+	 */
+	@Test
+	public void shouldPersistAndReadLinkByFullUrl() throws Exception {
+		// == prepare ==
+		String name = "Neo4j-Tutorial";
+		String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
+		String title = "Neo4j-Tutorial-Title";
 
-        // == go ==
-        classUnderTest.addLink(name, url, title);
-        List<Link> resultLinks = classUnderTest.getLink(LinkProperty.URL, url);
+		// == go ==
+		classUnderTest.addLink(name, url, title);
+		List<Link> resultLinks = classUnderTest.getLink(LinkProperty.URL, url);
 
-        // == verify ==
-        assertThat(resultLinks.size(), is(1));
-        Link resultLink = resultLinks.get(0);
-        assertThat(resultLink.getUrl(), is(url));
-        assertThat(resultLink.getTitle(), is(title));
-        assertThat(resultLink.getClicks(), is(0));
-        assertThat(resultLink.getScore(), is(0.0));
-    }
+		// == verify ==
+		assertThat(resultLinks.size(), is(1));
+		Link resultLink = resultLinks.get(0);
+		assertThat(resultLink.getUrl(), is(url));
+		assertThat(resultLink.getTitle(), is(title));
+		assertThat(resultLink.getClicks(), is(0));
+		assertThat(resultLink.getScore(), is(0.0));
+	}
 
-    /**
-     * @author mvogel
-     */
-    @Test
-    public void shouldPersistAndReadLinkBySubstringUrl() throws Exception {
-        // == prepare ==
-        String name = "Neo4j-Tutorial1";
-        String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-        String title = "Neo4j-Tutorial-Embedded";
+	/**
+	 * @author mvogel
+	 */
+	@Test
+	public void shouldPersistAndReadLinkBySubstringUrl() throws Exception {
+		// == prepare ==
+		String name = "Neo4j-Tutorial1";
+		String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
+		String title = "Neo4j-Tutorial-Embedded";
 
-        classUnderTest.addLink(name, url, title);
-        classUnderTest.addLink("Neo4j-Tutorial2", "http://neo4j.com/docs/stable/complete.html", "Neo4j");
-        classUnderTest.addLink("Neo4j-Tutorial3", "http://neo4j.com/docs/stable/half.html", "Neo4j");
-        classUnderTest.addLink("Linux", "http://linux.com", "Linux-Stuff");
-        classUnderTest.addLink("Apple-Repair", "http://applerepair.com", "Apple-Repair");
-        classUnderTest.addLink("Apple-Talk", "http://appletalk.com", "Apple-Talk");
+		classUnderTest.addLink(name, url, title);
+		classUnderTest.addLink("Neo4j-Tutorial2",
+				"http://neo4j.com/docs/stable/complete.html", "Neo4j");
+		classUnderTest.addLink("Neo4j-Tutorial3",
+				"http://neo4j.com/docs/stable/half.html", "Neo4j");
+		classUnderTest.addLink("Linux", "http://linux.com", "Linux-Stuff");
+		classUnderTest.addLink("Apple-Repair", "http://applerepair.com",
+				"Apple-Repair");
+		classUnderTest.addLink("Apple-Talk", "http://appletalk.com",
+				"Apple-Talk");
 
-        // == go ==
-        List<Link> resultLinks = classUnderTest.getLink(LinkProperty.URL, "neo4j.co");
+		// == go ==
+		List<Link> resultLinks = classUnderTest.getLink(LinkProperty.URL,
+				"neo4j.co");
 
-        // == verify ==
-        assertThat(resultLinks.size(), is(3));
-        resultLinks.forEach(System.out::println);
-    }
+		// == verify ==
+		assertThat(resultLinks.size(), is(3));
+		resultLinks.forEach(System.out::println);
+	}
 
-    /**
-     * @author mvogel
-     */
-    @Test
-    public void shouldUpdateTheNameOfTheLink() throws Exception {
-        // == prepare ==
-        String oldName = "Neo4j-Tutorial";
-        String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-        String title = "Neo4j-Tutorial-Title";
-        classUnderTest.addLink(oldName, url, title);
+	/**
+	 * @author mvogel
+	 */
+	@Test
+	public void shouldUpdateTheNameOfTheLink() throws Exception {
+		// == prepare ==
+		String oldName = "Neo4j-Tutorial";
+		String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
+		String title = "Neo4j-Tutorial-Title";
+		classUnderTest.addLink(oldName, url, title);
 
-        String newName = "Neo4j-New-Tutorial-111";
+		String newName = "Neo4j-New-Tutorial-111";
 
-        // == go ==
-        classUnderTest.updateLink(LinkProperty.NAME, oldName, newName);
+		// == go ==
+		classUnderTest.updateLink(LinkProperty.NAME, oldName, newName);
 
-        // == verify ==
-        List<Link> noResultLinks = classUnderTest.getLink(LinkProperty.NAME, oldName);
-        assertThat(noResultLinks.size(), is(0));
-        List<Link> resultLinks = classUnderTest.getLink(LinkProperty.NAME, newName);
-        assertThat(resultLinks.size(), is(1));
-        Link resultLink = resultLinks.get(0);
-        assertThat(resultLink.getName(), is(newName));
-        assertThat(resultLink.getUrl(), is(url));
-        assertThat(resultLink.getTitle(), is(title));
-        assertThat(resultLink.getClicks(), is(0));
-        assertThat(resultLink.getScore(), is(0.0));
-    }
+		// == verify ==
+		List<Link> noResultLinks = classUnderTest.getLink(LinkProperty.NAME,
+				oldName);
+		assertThat(noResultLinks.size(), is(0));
+		List<Link> resultLinks = classUnderTest.getLink(LinkProperty.NAME,
+				newName);
+		assertThat(resultLinks.size(), is(1));
+		Link resultLink = resultLinks.get(0);
+		assertThat(resultLink.getName(), is(newName));
+		assertThat(resultLink.getUrl(), is(url));
+		assertThat(resultLink.getTitle(), is(title));
+		assertThat(resultLink.getClicks(), is(0));
+		assertThat(resultLink.getScore(), is(0.0));
+	}
 
-    /**
-     * @author mvogel
-     */
-    @Test(expected = IllegalArgumentException.class)
-    public void shouldFailToUpdateTheNameOfTheLinkBecauseItDoesNotExist() throws Exception {
-        // == prepare ==
-        String oldName = "Neo4j-Tutorial";
-        String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-        String title = "Neo4j-Tutorial-Title";
-        classUnderTest.addLink(oldName, url, title);
+	/**
+	 * @author mvogel
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void shouldFailToUpdateTheNameOfTheLinkBecauseItDoesNotExist()
+			throws Exception {
+		// == prepare ==
+		String oldName = "Neo4j-Tutorial";
+		String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
+		String title = "Neo4j-Tutorial-Title";
+		classUnderTest.addLink(oldName, url, title);
 
-        String newName = "Neo4j-New-Tutorial-111";
+		String newName = "Neo4j-New-Tutorial-111";
 
-        // == go ==
-        try {
-            classUnderTest.updateLink(LinkProperty.NAME, "i do not exist", newName);
-            fail();
-        } catch (Exception e) {
-            // == verify ==
-            assertTrue(e.getMessage().contains("no node was found for"));
-            throw e;
-        }
-    }
-
-    // /**
-    // * @author mvogel
-    // */
-    // @Test
-    // public void shouldPersistAndReadTagToImpermanentEmbeddedDb() throws Exception {
-    // // == prepare ==
-    // String tag = "myTag";
-    //
-    // // == go ==
-    // classUnderTest.addTag(tag);
-    // String readTag = classUnderTest.findTag(tag);
-    //
-    // // == verify ==
-    // assertEquals(tag, readTag);
-    // }
-    //
-    //
-    // /**
-    // * @author mvogel
-    // */
-    // @Test
-    // @Ignore("fixme")
-    // public void shouldFindLinkToByTag() throws Exception {
-    // // == prepare ==
-    // String name = "myLink";
-    // String url = "http://neo4j.com/docs/stable/tutorials-java-embedded-hello-world.html";
-    // String tag = "neo4jTut";
-    //
-    // // == go ==
-    // classUnderTest.addLink(name, url);
-    // classUnderTest.addTag(tag);
-    // classUnderTest.tagLink(tag, url);
-    // List<String> foundLinks = classUnderTest.findLinksByTag(url);
-    //
-    // // == verify ==
-    // assertNotNull(foundLinks);
-    // assertEquals(1, foundLinks.size());
-    // assertEquals(url, foundLinks.get(0));
-    // }
-
-    // TODO tests for relations
+		// == go ==
+		try {
+			classUnderTest.updateLink(LinkProperty.NAME, "i do not exist",
+					newName);
+			fail();
+		} catch (Exception e) {
+			// == verify ==
+			assertTrue(e.getMessage().contains("no node was found for"));
+			throw e;
+		}
+	}
 }
