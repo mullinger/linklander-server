4576b34cf4d3b05e05a7c01eb9bb91bf8af980e7
max <max@ullinger.ch>
2015-10-18 14:35:18 +0200

    test

--- target/linklander/linklander.sh
+++ target/linklander/linklander.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ "$1" = "start" ] ; then
-	./tomee/bin/catalina.sh start
-elif [ "$1" = "stop" ] ; then
-	./tomee/bin/catalina.sh stop
-fi
+# Load config into the current shell environment
+source ./linklander-config.sh
+
+#if [ "$1" = "start" ] ; then
+	./tomee/bin/catalina.sh run
+#elif [ "$1" = "stop" ] ; then
+	#./tomee/bin/catalina.sh stop
+#fi
